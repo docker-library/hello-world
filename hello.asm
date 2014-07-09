b7a78b7ccca62cc478919b101f3ab1334899df2b
Tianon Gravi <admwiggin@gmail.com>
2014-07-09 14:25:09 -0600

    Update to save a few more bytes
    https://github.com/dotcloud/stackbrew/issues/102#issuecomment-48528203

--- hello.asm
+++ hello.asm
@@ -62,11 +62,11 @@ _start:
 		db 'To generate this message, Docker took the following steps:', 0x0A
 		db ' 1. The Docker client contacted the Docker daemon.', 0x0A
 		db ' 2. The Docker daemon pulled the "hello-world" image from the Docker Hub.', 0x0A
-		db '    (Assuming that it was not already locally available.)', 0x0A
+		db '    (Assuming it was not already locally available.)', 0x0A
 		db ' 3. The Docker daemon created a new container from that image which runs the', 0x0A
 		db '    executable that produces the output you are currently reading.', 0x0A
-		db ' 4. The Docker daemon streamed that output to the Docker client, which', 0x0A
-		db '    placed it in your terminal.', 0x0A
+		db ' 4. The Docker daemon streamed that output to the Docker client, which sent it', 0x0A
+		db '    to your terminal.', 0x0A
 		db 0x0A
 		db 'To try something more ambitious, you can run an Ubuntu container with:', 0x0A
 		db ' $ docker run -it ubuntu bash', 0x0A
