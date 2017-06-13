7d0ee592e4ed60e2da9d59331e16ecdcadc1ed87
Tianon Gravi <admwiggin@gmail.com>
2017-06-13 15:55:03 -0700

    Add initial multiarchitecture support via Debian cross-compiled binaries

--- update.sh
+++ update.sh
@@ -10,10 +10,11 @@ docker build -f Dockerfile.build -t hello-world:build .
 rm -rf */hello */nanoserver/hello.txt
 docker run --rm hello-world:build sh -c 'find \( -name hello -or -name hello.txt \) -print0 | xargs -0 tar --create' | tar --extract --verbose
 
-for h in */hello; do
+for h in amd64/*/hello; do
 	d="$(dirname "$h")"
+	b="$(basename "$d")"
 	"$h" > /dev/null
-	docker build -t hello-world:"test-$d" "$d"
-	docker run --rm hello-world:"test-$d"
+	docker build -t hello-world:"test-$b" "$d"
+	docker run --rm hello-world:"test-$b"
 done
-ls -l */nanoserver/hello.txt || :
+ls -lh */*/{hello,nanoserver/hello.txt} || :
