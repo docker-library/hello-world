7c49831fd4a723d39fa33be77dc1fdee49b2ad85
Tianon Gravi <admwiggin@gmail.com>
2018-07-09 16:14:42 -0700

    Add Nano Server 1803 variants

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -62,7 +62,7 @@ for arch in "${arches[@]}"; do
 	EOE
 done
 
-for winVariant in nanoserver-sac2016 nanoserver-1709; do
+for winVariant in nanoserver-{sac2016,1709,1803}; do
 	winArches=( *"/$image/$winVariant/hello.txt" )
 	winArches=( "${winArches[@]%"/$image/$winVariant/hello.txt"}" )
 
