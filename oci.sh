0688ddcaea0ad60cf988d1fb389e69f0e438d483
Laurent Goderre <laurent.goderre@docker.com>
2025-02-03 12:02:53 -0500

    fixup

--- oci.sh
+++ oci.sh
@@ -8,46 +8,26 @@ hash="sha256"
 self="$(basename "$BASH_SOURCE")"
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
-ociroot="./oci"
-blobs="$ociroot/blobs/$hash"
-
-rm -rf $ociroot
-
-mkdir -p $blobs
-
 toBlob() {
     local content="$1"; shift
+    local filename="$1"; shift
 
-    digest="$(echo -n $content | sha256sum | cut -d " " -f1)"
-    echo -n $content > "$blobs/$digest"
+    output="${ociroot}/$filename"
+    echo -n $content > "$output"
+    digest="$(cat $output | sha256sum | cut -d " " -f1)"
+    ln -s "../../$filename" "$blobs/$digest"
+    
     echo $digest
-}
-
-annotations() {
-    local os="$1"; shift
-    local arch="$1"; shift
-    local timestamp="$1"; shift
-    local revision="$1"; shift
-
-    jq -r -n --arg os $os --arg arch $arch --arg timestamp $timestamp --arg revision $revision '{
-        "com.docker.official-images.bashbrew.arch": $arch,
-        "org.opencontainers.image.base.name": "scratch",
-        "org.opencontainers.image.created": $timestamp,
-        "org.opencontainers.image.revision": $revision,
-        "org.opencontainers.image.source": "https://github.com/docker-library/hello-world.git#\($revision):\($arch)/hello-world",
-        "org.opencontainers.image.url": "https://hub.docker.com/_/hello-world",
-        "org.opencontainers.image.version": $os
-    } | tojson'
 }
 
 rootfs() {
     local os="$1"; shift
     local arch="$1"; shift
 
-    path="./$arch/hello-world"
-    tar -C $path -cf - hello > tmp.tar
-    digest="$(cat tmp.tar | sha256sum | cut -d " " -f1)"
-    mv tmp.tar "$blobs/$digest"
+    output="${ociroot}/rootfs.tar"
+    tar -cf - hello > $output
+    digest="$(cat $output | sha256sum | cut -d " " -f1)"
+    ln -s "../../rootfs.tar" "$blobs/$digest"
     echo $digest
 }
 
@@ -92,23 +72,21 @@ config() {
         "created": $timestamp,
     } | tojson')"
     
-    toBlob $content
+    toBlob $content "image-config.json"
 }
 
 manifest() {
     local configdigest="$1"; shift
     local rootfsdigest="$1"; shift
-    local annotations="$1"; shift
 
-    configsize="$(wc -c "$blobs/$configdigest" | awk '{print $1}')"
-    rootfssize="$(wc -c "$blobs/$rootfsdigest" | awk '{print $1}')"
+    configsize="$(wc -c "${ociroot}/image-config.json" | awk '{print $1}')"
+    rootfssize="$(wc -c "${ociroot}/rootfs.tar" | awk '{print $1}')"
 
     content="$(jq -r -n \
         --arg configdigest "$hash:$configdigest" \
         --arg configsize $configsize \
         --arg rootfsdigest "$hash:$rootfsdigest" \
         --arg rootfssize $rootfssize \
-        --argjson annotations $annotations \
         '{
         schemaVersion: 2,
         mediaType: "application/vnd.oci.image.manifest.v1+json",
@@ -124,19 +102,21 @@ manifest() {
                 size: $rootfssize | tonumber
             }
         ],
-        annotations: $annotations
+        annotations: {
+            "org.opencontainers.image.url": "https://hub.docker.com/_/hello-world",
+            "org.opencontainers.image.version": "linux"
+        }
     } | tojson')"
     
-    toBlob $content
+    toBlob $content "image-manifest.json"
 }
 
 indexdescriptor() {
     local manifestdigest="$1"; shift
     local os="$1"; shift
     local arch="$1"; shift
-    local annotations="$1"; shift
 
-    manifestsize="$(wc -c "$blobs/$manifestdigest" | awk '{print $1}')"
+    manifestsize="$(wc -c "${ociroot}/image-manifest.json" | awk '{print $1}')"
 
     platform=""
 
@@ -167,39 +147,47 @@ indexdescriptor() {
         --arg digest "$hash:$manifestdigest" \
         --arg manifestsize $manifestsize \
         --argjson platform $platform \
-        --argjson annotations $annotations \
         '{
         mediaType: "application/vnd.oci.image.manifest.v1+json",
         digest: $digest,
         size: $manifestsize | tonumber,
         platform: $platform,
-        annotations: $annotations
+        annotations: {
+            "org.opencontainers.image.ref.name": "hello-world:linux",
+			"io.containerd.image.name": "hello-world:linux"
+        }
     } | tojson'
 }
 
 created="2025-01-21T23:32:32Z"
 
-arches=( *"/$image/hello" )
-
-manifests="[]"
+arches=( *"/hello" )
 
 for path in "${arches[@]}"; do
     os="linux"
-    arch="${path/\/hello\-world\/hello/}"
+    arch="${path/\/hello/}"
 
-    annotations=$(annotations $os $arch $created "foo")
+    cd "$arch"
+
+    ociroot="./oci"
+    blobs="$ociroot/blobs/$hash"
+
+    rm -rf $ociroot
+
+    mkdir -p $blobs
+
     rootfs=$(rootfs $os $arch)
     config=$(config $rootfs $os $arch $created)
-    manifest=$(manifest $config $rootfs $annotations)
-    indexdescriptor=$(indexdescriptor $manifest $os $arch $annotations)
+    manifest=$(manifest $config $rootfs)
+    indexdescriptor=$(indexdescriptor $manifest $os $arch)
 
-    manifests=$(jq -r -n --argjson manifests $manifests --argjson manifest $indexdescriptor '$manifests  | . += [$manifest] | tojson')
+    jq -r -n --argjson manifests $indexdescriptor '{
+        schemaVersion: 2,
+        mediaType: "application/vnd.oci.image.index.v1+json",
+        manifests: [$manifests]
+    }' > "$ociroot/index.json"
+
+    cd -
+
+    # manifests=$(jq -r -n --argjson manifests $manifests --argjson manifest $indexdescriptor '$manifests  | . += [$manifest] | tojson')
 done
-
-# TODO: Windows
-
-jq -r -n --argjson manifests $manifests '{
-    schemaVersion: 2,
-    mediaType: "application/vnd.oci.image.index.v1+json",
-    manifests: $manifests
-}' > "$ociroot/index.json"
